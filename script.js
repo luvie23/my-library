666587adef1601cb8543f0bec8dd9c76b4b4a990
Luvie Rallos <luvierallos@gmail.com>
2022-10-14 09:55:06 +0800

    add remove and read/unread functionality

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let shelf = document.getElementById('shelf');
-let submission = document.get
-const shelves = document.querySelectorAll('.shelves')
-
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -13,9 +10,8 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function(){
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages`;
 }
-
 
 function addBookToLibrary(Book){
     myLibrary.push(Book);
@@ -26,36 +22,62 @@ function addBookToLibrary(Book){
 function setupShelf(){
         shelf.innerHTML = "";
         for (i = 0; i < myLibrary.length; i++) {
-            
             let element = document.createElement('div');
             let buttons = document.createElement('div');
             let button = document.createElement('button');
             let button2 = document.createElement('button');
+
             buttons.className = "buttons";
-            button.getAttribute('id','remove-button')
-            button2.getAttribute('id','read-button')
-            button2.className = "card-button"
+            
+            // create the remove button and initiate the functionality
+            button.setAttribute('id','remove-button'+i)
+            button.textContent = 'remove';
             button.className = "card-button";
+            button.className = "remove-button";
+            button.onclick = function(){
+                remove(button.getAttribute('id').charAt(button.getAttribute('id').length - 1)) 
+                //takes the value of i when the button was created and passes it to the remove function as the array index to be removed
+            }
+
+            button2.setAttribute('id','read-button'+i)
+            button2.textContent = 'read/unread'
+            button2.className = "card-button"
+            button2.onclick = function(){
+                    toggle(button2.getAttribute('id').charAt(button2.getAttribute('id').length - 1))
+            }
+
             element.textContent = myLibrary[i].info();
             element.setAttribute('id', "card"+i);
             element.className = "cards";
+            myLibrary[i].read ? element.classList.add('true'): element.classList.add('false');
+
             buttons.appendChild(button)
             buttons.appendChild(button2);
             element.appendChild(buttons);
             shelf.appendChild(element);
+         
+
         }
 }
-
-let buttons = document.querySelectorAll('button')
 
 document.getElementById('submit').onclick = function(){
     let title = document.getElementById('book-title').value;
     let author = document.getElementById('book-author').value;
     let page = document.getElementById('book-page').value;
-    let read = document.getElementById('book-read').value;
+    let read = document.getElementById('book-read').checked;
 
     let newBook = new Book(title, author, page, read);
     addBookToLibrary(newBook);
     setupShelf()
-    console.log(newBook)
+    
+}
+
+function remove(index){
+    myLibrary.splice(index,1)
+    setupShelf()
+}
+
+function toggle(index){
+    myLibrary[index].read ? myLibrary[index].read =  false : myLibrary[index].read =  true;
+    setupShelf()
 }
